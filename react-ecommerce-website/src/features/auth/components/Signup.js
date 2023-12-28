de8da5b0a991904053a4f3535444b6562efa18d2
Namrata Roy <royrio2000@gmail.com>
2023-12-28 16:33:32 +0530

    part 5
    Added Routing Functionality

--- react-ecommerce-website/src/features/auth/components/Signup.js
+++ react-ecommerce-website/src/features/auth/components/Signup.js
@@ -5,6 +5,7 @@
     incrementAsync,
     selectCount,
     } from '../authSlice';
+    import { Link } from 'react-router-dom';
 
     export default function Signup() {
     const count = useSelector(selectCount);
@@ -94,9 +95,9 @@
 
             <p className="mt-10 text-center text-sm text-gray-500">
                 Already a member?{' '}
-                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                 Log In
-                </a>
+                </Link>
             </p>
             </div>
         </div>
